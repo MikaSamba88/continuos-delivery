a15d8e4c4dfa964d9d3b99b34547fb88037a429e
mika samba <mika.samba@chasacademy.se>
2026-04-16 14:18:08 +0200

    add additional tests for API endpoints in test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,25 @@
 from fastapi.testclient import TestClient
-
 from main import app
 
 client = TestClient(app)
 
-def test_root():
+def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+def test_read_items():
+    response = client.get("/items/42")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 42, "q": "TEST"}
+
+def test_who():
+    response = client.get("/who")
+    assert response.status_code == 200
+    assert "hostname" in response.json()
+
+def test_random_gif():
+    response = client.get("/random-gif")
+    assert response.status_code == 200
+    assert "url" in response.json()
+    assert response.json()["url"].startswith("https://")

a6132319c4beccc285aace69445230c670486945
mika samba <mika.samba@chasacademy.se>
2026-04-19 18:29:15 +0200

    feat: update stack name format to include 'samba' prefix in deployment script

--- deploy.py
+++ deploy.py
@@ -8,7 +8,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # From Gitlab CI/CD
 PORTAINER_URL = os.getenv("PORTAINER_URL")
 API_KEY = os.getenv("PORTAINER_TOKEN")
-STACK_NAME = f"{os.getenv('CI_PROJECT_NAME', 'api')}-{os.getenv('CI_COMMIT_REF_SLUG', 'dev')}"
+STACK_NAME = f"samba-{os.getenv('CI_PROJECT_NAME')}-{os.getenv('CI_COMMIT_REF_SLUG')}"
 ENDPOINT_ID = 8
 # Load the compose-file
 COMPOSE_FILE = "docker-compose.yml" 
